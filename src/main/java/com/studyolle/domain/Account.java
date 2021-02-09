c2cb58451d32cf4f70417e87d7db2df8d93181ca
Kimginam97 <rlarlska79@naver.com>
2021-02-09 13:33:07 +0900

    1.1.관심주제도메인

--- src/main/java/com/studyolle/domain/Account.java
+++ src/main/java/com/studyolle/domain/Account.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -54,6 +55,8 @@ public class Account {
 
     private boolean studyUpdatedByWeb;
 
+    @ManyToMany
+    private Set<Tag> tags;
 
     public void generateEmailCheckToken() {
         this.emailCheckToken = UUID.randomUUID().toString();
