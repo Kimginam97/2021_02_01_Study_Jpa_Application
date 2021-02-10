34e381bc5b6cbde16b51f5e98d6ee45fc51d2eaa
Kimginam97 <rlarlska79@naver.com>
2021-02-10 15:52:03 +0900

    1.9.지역정보추가삭제및테스트

--- src/main/java/com/studyolle/zone/ZoneService.java
+++ src/main/java/com/studyolle/zone/ZoneService.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
@@ -21,7 +24,7 @@ public class ZoneService {
 
     private final ZoneRepository zoneRepository;
 
-    @PostConstruct
+    /*@PostConstruct
     public void initZoneData() throws IOException {
         if (zoneRepository.count() == 0) {
             Resource resource = new ClassPathResource("zones_kr.csv");
@@ -31,6 +34,23 @@ public class ZoneService {
                         return Zone.builder().city(split[0]).localNameOfCity(split[1]).province(split[2]).build();
                     }).collect(Collectors.toList());
             zoneRepository.saveAll(zoneList);
+        }
+    }*/
+
+    @PostConstruct
+    public void initZoneData() throws IOException {
+        if (zoneRepository.count() == 0) {
+            Resource resource = new ClassPathResource("zones_kr.csv");
+
+            InputStream zonesInputStream = resource.getInputStream();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(zonesInputStream)) ) {
+                List<Zone> zoneList = reader.lines().map(line -> {
+                    String[] split = line.split(",");
+                    return Zone.builder().city(split[0]).localNameOfCity(split[1]).province(split[2]).build();
+                }).collect(Collectors.toList());
+
+                zoneRepository.saveAll(zoneList);
+            }
         }
     }
 
